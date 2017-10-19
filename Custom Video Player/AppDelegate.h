b1e333a776f3cfb4043231bc3e6f5364e3be145e
Amir J <amir.jahan@gmail.com>
2017-10-19 11:35:01 -0700

    Initial Commit

---
+++ Custom Video Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custom Video Player
+//
+//  Created by Amir on 10/19/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Custom Video Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Custom Video Player
+//
+//  Created by Amir on 10/19/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
